2d7765d526b7549507a9daff1cfab58f353cf084
Jason <jasonnnnli1997@gmail.com>
2020-08-05 19:13:02 -0400

    1
    1

---
+++ review.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS Review;
+CREATE TABLE Review (Username VARCHAR(255) NOT NULL UNIQUE,
+                       Devicename VARCHAR(255) NOT NULL UNIQUE,
+                       Reviewcontent VARCHAR(255) NOT NULL ,
+                       Rating VARCHAR(255) NOT NULL,
+                       CONSTRAINT ReviewFK FOREIGN KEY (Username) REFERENCES members (Username),
+                      	CONSTRAINT ReviewFK FOREIGN KEY (Devicename) REFERENCES Device (Devicename)
+
+  );
