e7d29ba70cc4d04dacc564e6a03c2031f9165b7c
Jason <jasonnnnli1997@gmail.com>
2020-08-05 19:42:44 -0400

    21
    12

--- members.sql
+++ members.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS Members;
-CREATE TABLE Members (MemberID SERIAL PRIMARY KEY,
+CREATE TABLE Members (MemberID VARCHAR(255),
                         FirstName VARCHAR(255) NOT NULL,
   		              LastName VARCHAR(255) NOT NULL,
-                        Username VARCHAR(255) NOT NULL UNIQUE,
+                        Username VARCHAR(255) PRIMARY KEY,
                         Email VARCHAR(255) NOT NULL UNIQUE,
                         Password VARCHAR(255) NOT NULL,
                         SALT VARCHAR(255),
--- routes/listdevice.js
+++ routes/listdevice.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const db = require('../utilities/sqlconn.js');
-
-var router = express.Router();
-
-const bodyParser = require("body-parser");
-//This allows parsing of the body of POST requests, that are encoded in JSON
-router.use(bodyParser.json());
-router.get("/device", (req, res) => {
-
-    db.manyOrNone('SELECT * FROM device')
-        //If successful, run function passed into .then()
-        .then((data) => {
-            res.send({
-                success: true,
-                courses: data
-            });
-        }).catch((error) => {
-        console.log(error);
-        res.send({
-            success: false,
-            error: error
-        })
-    });
-});
-
-
-module.exports = router;
+// const express = require('express');
+// const db = require('../utilities/sqlconn.js');
+//
+// var router = express.Router();
+//
+// const bodyParser = require("body-parser");
+// //This allows parsing of the body of POST requests, that are encoded in JSON
+// router.use(bodyParser.json());
+// router.get("/device", (req, res) => {
+//
+//     db.manyOrNone('SELECT * FROM device')
+//         //If successful, run function passed into .then()
+//         .then((data) => {
+//             res.send({
+//                 success: true,
+//                 courses: data
+//             });
+//         }).catch((error) => {
+//         console.log(error);
+//         res.send({
+//             success: false,
+//             error: error
+//         })
+//     });
+// });
+//
+//
+// module.exports = router;
