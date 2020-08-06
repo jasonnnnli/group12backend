19e2a8e1475b8d1bf43fa8c2a270a491b2e09d87
Jason <jasonnnnli1997@gmail.com>
2020-08-05 22:07:48 -0400

    312
    321

--- devices.sql
+++ devices.sql
@@ -2,3 +2,7 @@ DROP TABLE IF EXISTS Devices;
 CREATE TABLE Devices (Devicename VARCHAR(255) PRIMARY KEY,
 Decicedetail VARCHAR(255) NOT NULL
   );
+  INSERT INTO Devices
+  VALUES('iphone', '1');
+    INSERT INTO Devices
+    VALUES('pixel', '1');
