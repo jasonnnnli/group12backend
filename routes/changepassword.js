805e16ea45fa50a72c318e5d0792ced5193896c0
Jason <jasonnnnli1997@gmail.com>
2020-08-17 15:53:46 -0400

    1
    1

--- routes/changepassword.js
+++ routes/changepassword.js
@@ -1,69 +1,100 @@
 //express is the framework we're going to use to handle requests
 const express = require('express');
-const crypto = require("crypto");
-let router = express.Router();
-let getHash = require('../utilities/utils').getHash;
-
-//Create connection to Heroku Database
-let db = require('../utilities/utils').db;
-
 
 const bodyParser = require("body-parser");
-//This allows parsing of the body of POST requests, that are encoded in JSON
+
+//We use this create the SHA256 hash
+const crypto = require("crypto");
+
+//Create connection to Heroku Database
+let db = require('../utilities/utilss').db;
+
+let getHash = require('../utilities/utilss').getHash;
+
+let sendEmail = require('../utilities/utilss').sendEmail;
+
+var router = express.Router();
 router.use(bodyParser.json());
 
+// Resets the users password given a valid email, password, and verification code
 router.post('/changepassword', (req, res) => {
-    let email = req.body['email'];
-    // let  firstname = req.body['firstname'];
-    // let lastname = req.body['lastname'];
-    // let username = req.body['username'];firstname &&lastname && username ,firstnamev=$2, lastname=$3,username=$4,[firstname],[lastname],[username]
-    let newPassword = req.body['newPassword'];
-    let wasSuccessful = false;
-    if(email && newPassword) {
-        //Using the 'one' method means that only one row should be returned
-        db.one('SELECT Email FROM Members WHERE Email=$1 ', [email])
-            .then(row => { //If successful, run function passed into .then()
-
-                var newPassword = req.body['newPassword'];
-
-                let salt = crypto.randomBytes(32).toString("hex");
-                let salted_hash = getHash(newPassword, salt);
-
-                let params = [salted_hash, salt, email];
-
-                db.none("UPDATE Members SET Password = $1, Salt = $2 WHERE Email = $3", params)
-                    .then(() => {
-                        return res.send({
-                            success: true,
-                            message: "Password changed"
-                        });
-
-
-                    }).catch((err) => {
-                    //log the error
-                    // Not sure why we would have an error here, we would have just made the user account
-                    res.send({
-                        success: false,
-
-                        error: "Couldn't update member's password"
-
-                    });
-                });
-
+    var email = req.body['email'].toLowerCase();
+    var password = req.body['password'];
+    var code = req.body['code'];
+    if (email && password && code) {
+        // If the email doesn't include an "@" sign
+        if (!email.includes("@")) {
+            res.send({
+                success: false,
+                error: "Email is invalid."
             })
-            //More than one row shouldn't be found, since table has constraint on it
-            .catch((err) => {
-                //If anything happened, it wasn't successful
-                res.send({
-                    success: false,
-                    message: "Account not found!"
-                });
-            });
-    } else {
+        }
+        else {
+            // Select the member with the given email
+            db.result(`SELECT * FROM MEMBERS
+                       WHERE EMAIL= $1`, [email])
+                .then(result => {
+                    // Get todays date
+                    var today = new Date();
+                    // Get the resetcode
+                    var resetcode = result.rows[0]["resetcode"];
+                    // Get the expiration of the code
+                    var expire = result.rows[0]["expire"];
+                    // If the code is expired
+                    if (!expire >= today) {
+                        res.send({
+                            success: false,
+                            error: "Code expired."
+                        })
+                    }
+                    else {
+                        // If the code doesn't match to the email provided
+                        if (code != resetcode) {
+                            res.send({
+                                success: false,
+                                error: "Code does not match to email provided."
+                            })
+                        }
+                        else {
+                            // Create a new hashed password
+                            let salt = crypto.randomBytes(32).toString("hex");
+                            let salted_hash = getHash(password, salt);
+                            // If the given password is too short
+                            if (password.length < 6) {
+                                res.send({
+                                    success: false,
+                                    error: "Password must be at least 6 characters long."
+                                });
+                            }
+                            else {
+                                // Update the database with the new information
+                                var params = [email, salted_hash, salt];
+                                db.none(`UPDATE MEMBERS 
+                                        SET Password = $2, Salt = $3, ResetCode = NULL, Expire = NULL 
+                                        WHERE EMAIL = $1`, params)
+                                    .then(() => {
+                                        res.send({
+                                            success: true
+                                        });
+                                        var message = "Your password has been updated.";
+                                        // Send a confirmation email
+                                        sendEmail(email, "Password Reset Confirmation", message);
+                                    })
+                            }
+                        }
+                    }
 
+
+                })
+
+
+        }
+        // If the email, password or code are not given.
+    } else {
         res.send({
             success: false,
-            message: 'Email ,fisrname,lastname,username and Password are required'
+            input: req.body,
+            error: "Missing required user information."
         });
     }
 });
---
+++ utilities/utilss.js
@@ -0,0 +1,72 @@
+//Get the connection to Heroku Database
+let db = require('./sql_conn.js');
+//We use this create the SHA256 hash
+const crypto = require("crypto");
+const FormData = require("form-data");
+var nodemailer = require('nodemailer');
+/**
+ * encrypt/decrypt found from : http://lollyrock.com/articles/nodejs-encryption/
+ */
+function encrypt(text, key){
+    var cipher = crypto.createCipher('aes-256-cbc',key)
+    var crypted = cipher.update(text,'utf8','hex')
+    crypted += cipher.final('hex');
+    return crypted.substring(0, 20);
+}
+
+function decrypt(text, key){
+    var decipher = crypto.createDecipher('aes-256-cbc',key)
+    var dec = decipher.update(text,'hex','utf8')
+    dec += decipher.final('utf8');
+    return dec;
+}
+
+/**
+ * Function to send emails. Derived from : https://www.w3schools.com/nodejs/nodejs_email.asp
+ */
+function sendEmail(receiving, subject, message)
+{
+    console.log("Message: " + message);
+    db.one("SELECT Encrypted, Email, Key FROM GMAIL")
+        .then(row => {
+            let key = row['key'];
+            let password = decrypt(row['encrypted'], key);
+            let email = row['email'];
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: email,
+                    pass: password
+                }
+            });
+
+            var mailOptions = {
+                sending: email,
+                to: receiving,
+                subject: subject,
+                text: message
+            };
+
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent.');
+                }
+            });
+        });
+}
+
+/**
+ * Method to get a salted hash.
+ * We put this in its own method to keep consistency
+ * @param {string} pw the password to hash
+ * @param {string} salt the salt to use when hashing
+ */
+function getHash(pw, salt) {
+    return crypto.createHash("sha256").update(pw + salt).digest("hex");
+}
+
+module.exports = {
+    db, getHash, sendEmail
+};
