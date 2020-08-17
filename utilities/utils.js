6009c6157849150fb643d22df45d25e453adc31a
Jason <jasonnnnli1997@gmail.com>
2020-08-17 18:12:05 -0400

    1
    1

--- utilities/utils.js
+++ utilities/utils.js
@@ -1,37 +1,28 @@
-let db = require('./sql_conn.js');
+//Get the connection to Heroku Database
+
+let db = require('./sqlconn.js');
+
+//We use this create the SHA256 hash
 const crypto = require("crypto");
-var nodemailer = require('nodemailer');
 
-function sendEmail(receiving, subject, message)
-{
-    console.log("Message: " + message);
+function sendEmail(from, receiver, subj, message) {
+    //research nodemailer for sending email from node.
+    // https://nodemailer.com/about/
+    // https://www.w3schools.com/nodejs/nodejs_email.asp
+    //create a burner gmail account
+    //make sure you add the password to the environmental variables
+    //similar to the DATABASE_URL and PHISH_DOT_NET_KEY (later section of the lab)
 
-
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'jasonnnnli1997@gmail.com',
-            pass: '97102401a.'
-        }
-    });
-
-            var mailOptions = {
-                sending: 'jasonnnnli1997@gmail.com',
-                to: receiving,
-                subject: subject,
-                text: message
-            };
-
-            transporter.sendMail(mailOptions, function(error, info){
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent.');
-                }
-            });
-
+    //fake sending an email for now. Post a message to logs.
+    console.log('Email sent: ' + message);
 }
 
+/**
+ * Method to get a salted hash.
+ * We put this in its own method to keep consistency
+ * @param {string} pw the password to hash
+ * @param {string} salt the salt to use when hashing
+ */
 function getHash(pw, salt) {
     return crypto.createHash("sha256").update(pw + salt).digest("hex");
 }
