0db73b142a862c276cdf8291544e151436b368a6
Jason <jasonnnnli1997@gmail.com>
2020-08-18 14:03:34 -0400

    1
    1

--- routes/login.js
+++ routes/login.js
@@ -47,7 +47,7 @@ router.post('/', (req, res) => {
                     //package and send the results
                     res.json({
                         success: true,
-                        message: 'Authentication successful!',
+                        message: row['Username'],
                         token: token
                     });
                 } else {
